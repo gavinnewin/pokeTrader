147d5f248e2d8ad5726417588ef55220e221231f
Antony Ibrahim <antonyi1@uci.edu>
2025-05-09 16:40:17 -0700

    better preview for home and search

--- src/data.js
+++ src/data.js
@@ -1,11 +1,11 @@
 // src/data.js
 export const featuredData = [
-  { id: 1, image: "/cards/unbroken.png", name: "Unbroken", price: "Near Mint • $31" },
+  { id: 1, image: "/poncho-pikachu.png", name: "Poncho-wearing Pikachu", price: "Near Mint • $31" },
   /* … */
 ];
 
 export const trendingData = [
-  { id: 101, image: "/cards/pikachu.png", name: "Pikachu", price: "Near Mint • $14" },
+  { id: 101, image: "/basic_giratina.png", name: "Giratina", price: "Near Mint • $1000" },
   /* … */
 ];
 
