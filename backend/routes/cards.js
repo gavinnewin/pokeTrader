9f34aba75eb56426720d216dd45633d3f4f75417
rafik <rsmena@uci.edu>
2025-05-31 15:27:24 -0700

    Update cards.js

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -18,15 +18,19 @@ router.post('/', async (req, res) => {
 // GET a single card by name (e.g., /api/cards/pikachu)
 router.get('/:name', async (req, res) => {
   try {
-    const card = await Card.findOne({ name: req.params.name});
+    const nameQuery = new RegExp(`^${req.params.name}$`, 'i'); // case-insensitive exact match
+    const card = await Card.findOne({ name: nameQuery });
+
     if (!card) {
       return res.status(404).json({ message: 'Card not found' });
     }
+
     res.json(card);
   } catch (err) {
     res.status(500).json({ message: 'Server error' });
   }
 });
+
 
 
 module.exports = router;
